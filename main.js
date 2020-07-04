1aa0166129494d8b661a6eb771dd7d33e98ddb4e
Xyrai <Xyraidev@gmail.com>
2020-07-04 16:53:35 +0200

    Added random colors

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const numRows = getComputedStyle(root).getPropertyValue('--numberOfRows');
 for (let i = 0; i < numColumns * numRows; i++) {
   const gridItem = document.createElement('div');
   gridItem.id = 'grid-item';
-  // gridItem.style.backgroundColor = "#"+((1<<24)*Math.random()|0).toString(16);
   gridItem.addEventListener("mouseenter", event =>
   event.target.style.backgroundColor = "black"
   );
@@ -15,14 +14,25 @@ for (let i = 0; i < numColumns * numRows; i++) {
 }
 
 const clearBtn = document.querySelector('#clear-btn');
+let items = document.querySelectorAll('#grid-item');
 
 clearBtn.addEventListener('click', () => {
-  let items = document.querySelectorAll('#grid-item');
 
   items.forEach(item => {
     if (item.style.backgroundColor != "") {
       item.style.backgroundColor = "white";
     }
+  });
+});
+
+const rainbowBtn = document.querySelector('#random-color-btn');
+
+rainbowBtn.addEventListener('click', () => {
+
+  items.forEach(item => {
+    item.addEventListener("mouseenter", event =>
+    event.target.style.backgroundColor = "#"+((1<<24)*Math.random()|0).toString(16)
+    );
   });
 });
 
